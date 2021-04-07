80ecfa7df74c227197efd6c2af9dfb3486d297f4
2021066708 <rladnjsry837@hayang.ac.kr>
2021-04-07 23:39:45 +0900

    revise

--- 5-1/5/5.py
+++ 5-1/5/5.py
@@ -9,26 +9,18 @@ def getRandomString(leng):
 
     return string
 
-def joinList(l, space=''):
-    string=''
-    for i, c in enumerate(l):
-        if i > 0:
-            string=string+space+c
-        else:
-            string=string+c
-    return string
 
 english='a b c d e f g h i j k l m n o p q r s t u v w x y z'.split()
 
 n = int(input('Input the length of the string : '))
-cheese = list(getRandomString(n))
-original = joinList(cheese)
-cnt, check = 0, []
+cheese = getRandomString(n)
+original = cheese
+cnt, check = 0, ''
 eaten = 0
 
 print(f"Generated Cheese is '{original}'")
 print('Mouse starts eating!')
-while cnt < 10:
+while cnt < 10 and eaten < n:
     cnt += 1
     eat = None
 
@@ -36,25 +28,26 @@ while cnt < 10:
         if eat is None or eat in check:
             eat = getRandomString(1)
         else:
-            check.append(eat)
+            check += (eat + ' ')
             break
 
-    cheese = ['_' if x == eat else x for x in cheese]
+    for c in cheese:
+        if c == eat:
+            cheese = cheese.replace(eat, '_', 1)
+            eaten += 1
 
     print(f"Start eating '{eat}'")
-    print(f'Eaten alphabet of cheese : {joinList(check, space=" ")}')
+    print(f'Eaten alphabet of cheese : {check}')
     print(f'Original cheese : {original}')
-    print(f'Current cheese status : {joinList(cheese)}')
+    print(f'Current cheese status : {cheese}')
     print()
 
-for x in cheese:
-    if x == '_':
-        eaten += 1
 if eaten == n:
     print('Out of cheese!')
 else:
     print('Finally remained Cheese Status :')
-    print(f'Eaten alphabet of cheese : {joinList(check, space=" ")}')
+    print(f'Eaten alphabet of cheese : {check}')
     print(f'Original cheese : {original}')
-    print(f'Current cheese status : {joinList(cheese)}')
+    print(f'Current cheese status : {cheese}')
+
 
