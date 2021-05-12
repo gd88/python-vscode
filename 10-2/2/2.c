b60cd85b5c72b3b089607fce96c94c044dbffb20
2021066708 <rladnjsry837@hayang.ac.kr>
2021-05-12 21:18:57 +0900

    10-2 revise

--- 10-2/2/2.c
+++ 10-2/2/2.c
@@ -5,16 +5,16 @@ int main()
     int a[5];
     scanf("%d %d %d %d %d", &a[0], &a[1], &a[2], &a[3], &a[4]);
     int* pa=a;
-    int b=*a; int c=*(a+1);
-    *(a)=*(a+4);
-    *(a+1)=*(a+3);
-    *(a+3)=c;
-    *(a+4)=b;
+    int b=*pa; int c=*(pa+1);
+    *(pa)=*(pa+4);
+    *(pa+1)=*(pa+3);
+    *(pa+3)=c;
+    *(pa+4)=b;
 
 
 
    
-    printf("%d %d %d %d %d", a[0], a[1], a[2], a[3], a[4]);
+    printf("%d %d %d %d %d", *pa, *(pa+1), *(pa+2), *(pa+3), *(pa+4));
     return 0;
 
 }
