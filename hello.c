a7b400c8482589a12f8de86834ccaaa39d41cf22
wongyo Kim <dnjsry8377@naver.com>
2022-01-21 21:10:00 +0900

    first

--- hello.c
+++ hello.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+
 int main()
 {
-    printf("hello\n"):
+    char name[10]="";
+    int count=0;
+    while(count<10)
+    {count=count+1;
+    scanf("%s", name);
+    printf("%s",name);
+    }
     return 0;
+
 }
